e69d0231270201ecc2a57e9416d9692a9fd3c443
Chris Carrington <chris@megafarad.com>
2025-08-23 17:02:42 -0400

    (feat) GitHub Actions CI/CD (fix) Improved secrets security

--- test/middleware.test.ts
+++ test/middleware.test.ts
@@ -31,18 +31,18 @@ describe('middleware', () => {
     it('should return 200/Okay when credentials are provided and authorized', async () => {
         const app = buildApp([]);
         await request(app).get('/tenant/1/secured').set('Authorization',
-            'Bearer test123').expect(200).expect({message: 'Hello World'});
+            `Bearer ${process.env.DEMO_BEARER_TOKEN!}`).expect(200).expect({message: 'Hello World'});
     });
 
     it('should return 400/Bad Request when tenantId is not an integer', async () => {
         const app = buildApp([]);
         await request(app).get('/tenant/invalid/secured').set('Authorization',
-            'Bearer test123').expect(400).expect({error: 'Invalid tenantId'});
+            `Bearer ${process.env.DEMO_BEARER_TOKEN!}`).expect(400).expect({error: 'Invalid tenantId'});
     });
 
     it('should authorize a service key', async () => {
         const app = buildApp(['appointments:getAvailable']);
         await request(app).get('/tenant/1/secured').set('X-API-Key',
-            'mfk_KhPUM7ro.VtD62UlBYwrMGyyDLSOSsDcA1qIAvSS21hfJUXmhqikGLk2KHO2n5Tg1P2Ul6IFG').expect(200).expect({message: 'Hello World'});
+            process.env.DEMO_API_KEY!).expect(200).expect({message: 'Hello World'});
     });
 });
